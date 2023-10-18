be9afa0118551460a6ea4a41a96fd9ed7f4df8d1
Andrew-R-Y <rakitinandrei@gmail.com>
2023-10-18 21:38:21 +0300

    feat: solve all arrays tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -664,8 +664,9 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  const result = arr.map((item) => childrenSelector(item));
+  return result.flat();
 }
 
 
@@ -681,8 +682,16 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  let result;
+  if (indexes.length > 0) {
+    const index = indexes.shift();
+    const newArr = arr[index];
+    result = getElementByIndexes(newArr, indexes);
+  } else {
+    result = arr;
+  }
+  return result;
 }
 
 
@@ -704,8 +713,20 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const movingLength = Math.floor(arr.length / 2);
+  const head = arr.slice(0, movingLength);
+  const tail = arr.slice(-movingLength);
+  let result = tail;
+  if (arr.length % 2 === 1) {
+    const middle = arr[movingLength];
+    result = tail.concat(middle);
+  }
+  result = result.concat(head);
+  if (arr.length === 1) {
+    result = arr;
+  }
+  return result;
 }
 
 
